4effc297c388cd23a7f23caee45b2b5cadecaf3c
GitHub Action <action@github.com>
2025-04-26 21:30:56 +0000

    Update Rust documentation

--- docs/hello_world/search-index.js
+++ docs/hello_world/search-index.js
@@ -1,4 +1,4 @@
-var searchIndex = new Map(JSON.parse('[["hello_world",{"t":"H","n":["main"],"q":[[0,"hello_world"]],"i":"`","f":"{{}b}","D":"`","p":[[1,"unit"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA=","P":[]}]]'));
+var searchIndex = new Map(JSON.parse('[["hello_world",{"t":"H","n":["main"],"q":[[0,"hello_world"],[1,"core::error"],[2,"alloc::boxed"],[3,"core::result"]],"i":"`","f":"{{}{{h{b{f{d}}}}}}","D":"`","p":[[1,"unit"],[10,"Error",1],[5,"Box",2,null,1],[6,"Result",3,null,1]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAEAAAAAAAEAEAAAAAAAAQA=","P":[]}]]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
-//{"start":39,"fragment_lengths":[182]}
+//{"start":39,"fragment_lengths":[307]}
