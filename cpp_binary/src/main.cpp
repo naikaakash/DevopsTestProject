c0713aeb33aaed88532aeec7c84eb9ccac1e69fe
aakashnaik <aakash_a_naik@yahoo.com>
2025-04-26 02:24:03 +0000

    update binaries

---
+++ cpp_binary/src/main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio>
+#include <string>
+#include <iostream>  // Needed for cout
+
+int main(int argc, char* argv[]) {
+    std::cout << "Hello World!";  // std::cout to use the correct namespace
+    return 0;
+}
